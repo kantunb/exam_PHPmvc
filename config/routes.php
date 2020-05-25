94c4682b974b677b1db3b6226dd37166507e64f7
Quentin Brouillet <quent07@hotmail.com>
2020-05-25 16:48:59 +0200

    update routes

--- config/routes.php
+++ config/routes.php
@@ -21,11 +21,19 @@ $router->get('/vehicule/(\d+)/delete', 'VehiculeController@delete');
 
 $router->get('/conducteur', 'ConducteurController@create');
 $router->post('/conducteur', 'ConducteurController@new');
+$router->get('/conducteur/(\d+)/edit', 'ConducteurController@edit');
+$router->post('/conducteur/(\d+)/edit', 'ConducteurController@update');
+$router->get('/conducteur/(\d+)/delete', 'ConducteurController@delete');
+
 
 // Associate table route
 
 $router->get('/association_vehicule_conducteur', 'AssociationVehiculeConducteurController@create');
 $router->post('/association_vehicule_conducteur', 'AssociationVehiculeConducteurController@new');
+$router->get('/association_vehicule_conducteur/(\d+)/edit', 'AssociationVehiculeConducteurController@edit');
+$router->post('/association_vehicule_conducteur/(\d+)/edit', 'AssociationVehiculeConducteurController@update');
+$router->get('/association_vehicule_conducteur/(\d+)/delete', 'AssociationVehiculeConducteurController@delete');
+
 
 // Queries route
 
